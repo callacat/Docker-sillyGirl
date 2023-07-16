b1424d05ec4f56ef9586e9e68367a8421590d7d0
callacat <wang892947707@hotmail.com>
2023-07-16 12:50:11 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# 启动sillyGirl程序并监控输出
+start_sillyGirl() {
+    echo "Starting sillyGirl..."
+    /usr/local/sillyGirl/sillyGirl -t
+}
+
 # 重启当前容器
 restart_container() {
-    echo "不进入静默模式，重启当前容器..."
+    echo "Restarting container..."
     sleep 3
     kill -s TERM 1
 }
@@ -11,16 +17,17 @@ restart_container() {
 monitor_output() {
     echo "进入监控程序..."
     /usr/local/sillyGirl/sillyGirl -t | while IFS= read -r line; do
-        echo "$line"  # 输出到标准输出（stdout）
-        if echo "$line" | grep -q "程序以静默模式运行"; then
-            restart_container  # 出现指定字样时重启当前容器
-        fi
-    done 
+    echo "$line" # 输出到标准输出（stdout）
+    if echo "$line" | grep -q "程序以静默模式运行"; then
+        restart_container # 出现指定字样时重启当前容器
+    fi
+    done
 }
 
 # 主函数
 main() {
-    monitor_output  # 启动监控程序输出并执行操作
+    start_sillyGirl &
+    monitor_output # 启动监控程序输出并执行操作
 }
 
 main "$@"
