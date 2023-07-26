772761dec448243c62e584cc4deeb132e0099d29
callacat <wang892947707@hotmail.com>
2023-07-26 20:45:45 +0800

    fix

--- monitorTest.sh
+++ monitorTest.sh
@@ -4,7 +4,7 @@
 start_sillyGirl() {
     ln -s /etc/sillyplus/language /usr/local/sillyGirl/language
     ln -s /etc/sillyplus/plugins /usr/local/sillyGirl/plugins
-    exec /etc/sillyplus/sillyGirl -t
+    exec /usr/local/sillyGirl/sillyGirl -t
 }
 
 # 重启当前容器
@@ -15,7 +15,7 @@ restart_container() {
 # 监控程序输出并执行操作
 monitor_output() {
     # echo "进入监控程序..."
-    /etc/sillyplus/sillyGirl -t | while IFS= read -r line; do
+    /usr/local/sillyGirl/sillyGirl -t | while IFS= read -r line; do
         echo "$line" # 输出到标准输出（stdout）
         if echo "$line" | grep -q "程序以静默模式运行"; then
             restart_container # 出现指定字样时重启当前容器
