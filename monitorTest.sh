c39b571cd7170177be54eeb4ac98cc3662e86a48
callacat <wang892947707@hotmail.com>
2023-08-10 22:18:20 +0800

    test

--- monitorTest.sh
+++ monitorTest.sh
@@ -2,40 +2,41 @@
 
 # 检查并创建目标路径
 prepare_target_paths() {
-    # if [ ! -d "/etc/sillyplus" ]; then
-    #     echo "/etc/sillyplus 目录不存在，开始创建"
-    #     mkdir -p /etc/sillyplus
-    # fi
+    prepare_directory "/etc/sillyplus/language"
+    prepare_directory "/etc/sillyplus/plugins"
 
-    # chmod 777 /etc/sillyplus
+    create_symbolic_link "/etc/sillyplus/language" "/usr/local/sillyGirl/language"
+    create_symbolic_link "/etc/sillyplus/plugins" "/usr/local/sillyGirl/plugins"
+}
 
-    if [ ! -d "/etc/sillyplus/language" ]; then
-        echo "/etc/sillyplus/language 目录不存在，开始创建"
-        mkdir -p /etc/sillyplus/language
+# 检查并创建目录
+prepare_directory() {
+    local dir="$1"
+    if [ ! -d "$dir" ]; then
+        mkdir -p "$dir"
+        # echo "初次使用，开始初始化目录"
     fi
+    chmod 755 "$dir" # 赋予必要的权限
+}
 
-    if [ ! -d "/etc/sillyplus/plugins" ]; then
-        echo "/etc/sillyplus/plugins 目录不存在，开始创建"
-        mkdir -p /etc/sillyplus/plugins
+# 创建符号链接
+create_symbolic_link() {
+    local source="$1"
+    local target="$2"
+    if [ ! -L "$target" ]; then
+        ln -s "$source" "$target"
+        echo "初次使用，开始初始化依赖目录"
     fi
-
-    chmod 777 /etc/sillyplus/language /etc/sillyplus/plugins
-
-    if [ ! -L "/usr/local/sillyGirl/language" ]; then
-        echo "指向 usr/local/sillyGirl/language 的软链接不存在，开始创建"
-        ln -s /etc/sillyplus/language /usr/local/sillyGirl/
-    fi
-
-    if [ ! -L "/usr/local/sillyGirl/plugins" ]; then
-        echo "指向 usr/local/sillyGirl/plugins 的软链接不存在，开始创建"
-        ln -s /etc/sillyplus/plugins /usr/local/sillyGirl/
-    fi
-
 }
 
 # 启动sillyGirl程序并监控输出
-start_sillyGirl() {
-    exec /usr/local/sillyGirl/sillyGirl -t
+start_and_monitor_sillyGirl() {
+    /usr/local/sillyGirl/sillyGirl -t | while IFS= read -r line; do
+        echo "$line"
+        if echo "$line" | grep -q "程序以静默模式运行"; then
+            restart_container
+        fi
+    done
 }
 
 # 重启当前容器
@@ -43,22 +44,10 @@ restart_container() {
     kill -s TERM 1
 }
 
-# 监控程序输出并执行操作
-monitor_output() {
-    # echo "进入监控程序..."
-    /usr/local/sillyGirl/sillyGirl -t | while IFS= read -r line; do
-        echo "$line" # 输出到标准输出（stdout）
-        if echo "$line" | grep -q "程序以静默模式运行"; then
-            restart_container # 出现指定字样时重启当前容器
-        fi
-    done
-}
-
 # 主函数
 main() {
-    prepare_target_paths # 检查并创建目标路径
-    start_sillyGirl &
-    monitor_output # 启动监控程序输出并执行操作
+    prepare_target_paths
+    start_and_monitor_sillyGirl
 }
 
 main "$@"
