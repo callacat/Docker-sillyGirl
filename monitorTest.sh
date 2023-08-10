83a981673ee714461defcb8961f55ff3974df20b
callacat <wang892947707@hotmail.com>
2023-08-11 00:33:31 +0800

    恢复正常软链接方式，注销停止函数

--- monitorTest.sh
+++ monitorTest.sh
@@ -2,30 +2,26 @@
 
 # 检查并创建目标路径
 prepare_target_paths() {
-    prepare_directory "/etc/sillyplus/language"
-    prepare_directory "/etc/sillyplus/plugins"
-
-    create_symbolic_link "/etc/sillyplus/*" "/usr/local/sillyGirl/"
-    # create_symbolic_link "/etc/sillyplus/plugins" "/usr/local/sillyGirl/"
-}
-
-# 检查并创建目录
-prepare_directory() {
-    local dir="$1"
-    if [ ! -d "$dir" ]; then
-        mkdir -p "$dir"
-        echo "初次使用，开始初始化目录 $dir"
+    if [ ! -d "/etc/sillyplus/language" ]; then
+        echo "/etc/sillyplus/language 目录不存在，开始创建"
+        mkdir -p /etc/sillyplus/language
     fi
-    chmod 755 "$dir" # 赋予必要的权限
-}
 
-# 创建符号链接
-create_symbolic_link() {
-    local source="$1"
-    local target="$2"
-    if [ ! -L "$target" ]; then
-        ln -s "$source" "$target"
-        echo "初次使用，开始初始化软链接"
+    if [ ! -d "/etc/sillyplus/plugins" ]; then
+        echo "/etc/sillyplus/plugins 目录不存在，开始创建"
+        mkdir -p /etc/sillyplus/plugins
+    fi
+
+    chmod 777 /etc/sillyplus/language /etc/sillyplus/plugins
+
+    if [ ! -L "/usr/local/sillyGirl/language" ]; then
+        echo "指向 usr/local/sillyGirl/language 的软链接不存在，开始创建"
+        ln -s /etc/sillyplus/language /usr/local/sillyGirl/
+    fi
+
+    if [ ! -L "/usr/local/sillyGirl/plugins" ]; then
+        echo "指向 usr/local/sillyGirl/plugins 的软链接不存在，开始创建"
+        ln -s /etc/sillyplus/plugins /usr/local/sillyGirl/
     fi
 }
 
@@ -34,20 +30,20 @@ start_and_monitor_sillyGirl() {
     /usr/local/sillyGirl/sillyGirl -t | while IFS= read -r line; do
         echo "$line"
         if echo "$line" | grep -q "程序以静默模式运行"; then
-            restart_container
+            echo "触发重启"
+            # restart_container
         fi
     done
 }
 
 # 重启当前容器
-restart_container() {
-    # 从 /etc/sillyplus/sillyGirl.pid 文件中读取进程 ID
-    pid=$(cat /etc/sillyplus/sillyGirl.pid)
+# restart_container() {
+#     # 从 /etc/sillyplus/sillyGirl.pid 文件中读取进程 ID
+#     pid=$(cat /etc/sillyplus/sillyGirl.pid)
 
-    # 发送 TERM 信号给指定的进程，让它优雅地终止
-    kill -s TERM $pid
-}
-
+#     # 发送 TERM 信号给指定的进程，让它优雅地终止
+#     kill -s TERM $pid
+# }
 
 # 主函数
 main() {
