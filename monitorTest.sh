c934475bd3fa9a80cb8eca626147a7076f4ee071
callacat <wang892947707@hotmail.com>
2023-08-11 02:08:38 +0800

    取消自定义npm和yarn源，优化初始化脚本脚本

--- monitorTest.sh
+++ monitorTest.sh
@@ -2,64 +2,40 @@
 
 # 检查并创建目标路径
 prepare_target_paths() {
-    # if [ ! -d "/etc/sillyplus" ]; then
-    #     echo "/etc/sillyplus 目录不存在，开始创建"
-    #     mkdir -p /etc/sillyplus
-    # fi
-
-    # chmod 777 /etc/sillyplus
-
     if [ ! -d "/etc/sillyplus/language" ]; then
-        echo "/etc/sillyplus/language 目录不存在，开始创建"
+        # echo "/etc/sillyplus/language 目录不存在，开始创建"
         mkdir -p /etc/sillyplus/language
     fi
 
     if [ ! -d "/etc/sillyplus/plugins" ]; then
-        echo "/etc/sillyplus/plugins 目录不存在，开始创建"
+        # echo "/etc/sillyplus/plugins 目录不存在，开始创建"
         mkdir -p /etc/sillyplus/plugins
     fi
 
     chmod 777 /etc/sillyplus/language /etc/sillyplus/plugins
 
     if [ ! -L "/usr/local/sillyGirl/language" ]; then
-        echo "指向 usr/local/sillyGirl/language 的软链接不存在，开始创建"
+        # echo "指向 usr/local/sillyGirl/language 的软链接不存在，开始创建"
         ln -s /etc/sillyplus/language /usr/local/sillyGirl/
     fi
 
     if [ ! -L "/usr/local/sillyGirl/plugins" ]; then
-        echo "指向 usr/local/sillyGirl/plugins 的软链接不存在，开始创建"
+        # echo "指向 usr/local/sillyGirl/plugins 的软链接不存在，开始创建"
         ln -s /etc/sillyplus/plugins /usr/local/sillyGirl/
     fi
-
 }
 
-# 启动sillyGirl程序并监控输出
-start_sillyGirl() {
-    exec /usr/local/sillyGirl/sillyGirl -t
-}
-
-# 重启当前容器
-restart_container() {
-    echo "触发重启"
-    # kill -s TERM 1
-}
-
-# 监控程序输出并执行操作
-monitor_output() {
-    # echo "进入监控程序..."
+# 启动sillyGirl程序并显示到标准输出
+start_and_monitor_sillyGirl() {
     /usr/local/sillyGirl/sillyGirl -t | while IFS= read -r line; do
         echo "$line" # 输出到标准输出（stdout）
-        if echo "$line" | grep -q "程序以静默模式运行"; then
-            restart_container # 出现指定字样时重启当前容器
-        fi
     done
 }
 
 # 主函数
 main() {
-    prepare_target_paths # 检查并创建目标路径
-    start_sillyGirl &
-    monitor_output # 启动监控程序输出并执行操作
+    prepare_target_paths
+    start_and_monitor_sillyGirl
 }
 
 main "$@"
