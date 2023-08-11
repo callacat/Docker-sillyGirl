770e6cf073111207f99ccf221a834c12b0cc5037
callacat <wang892947707@hotmail.com>
2023-08-11 23:40:08 +0800

    update

--- monitor.sh
+++ monitor.sh
@@ -1,30 +1,43 @@
 #!/bin/sh
 
+# 检查并创建目标路径
+prepare_target_paths() {
+    if [ ! -d "/etc/sillyplus/language" ]; then
+        mkdir -p /etc/sillyplus/language
+    fi
+
+    if [ ! -d "/etc/sillyplus/plugins" ]; then
+        mkdir -p /etc/sillyplus/plugins
+    fi
+
+    chmod 777 /etc/sillyplus/language /etc/sillyplus/plugins
+
+    if [ ! -L "/usr/local/sillyGirl/language" ]; then
+        ln -s /etc/sillyplus/language /usr/local/sillyGirl/
+    fi
+
+    if [ ! -L "/usr/local/sillyGirl/plugins" ]; then
+        ln -s /etc/sillyplus/plugins /usr/local/sillyGirl/
+    fi
+}
+
 # 启动sillyGirl程序并监控输出
 start_sillyGirl() {
     exec /usr/local/sillyGirl/sillyGirl -t
 }
 
-# 重启当前容器
-restart_container() {
-    kill -s TERM 1
-}
-
 # 监控程序输出并执行操作
 monitor_output() {
-    # echo "进入监控程序..."
     /usr/local/sillyGirl/sillyGirl -t | while IFS= read -r line; do
         echo "$line" # 输出到标准输出（stdout）
-        if echo "$line" | grep -q "程序以静默模式运行"; then
-            restart_container # 出现指定字样时重启当前容器
-        fi
     done
 }
 
 # 主函数
 main() {
+    prepare_target_paths
     start_sillyGirl &
-    monitor_output # 启动监控程序输出并执行操作
+    monitor_output
 }
 
 main "$@"
